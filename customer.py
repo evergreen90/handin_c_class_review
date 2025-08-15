549a578f1edbf83db978768b2f9a53fcdef6c3fe
evergreen90 <evergreen90@gmail.com>
2025-08-15 11:05:20 +0900

    featureでC-6まで回答作成

--- customer.py
+++ customer.py
@@ -1,28 +1,77 @@
 # 下記の雛形を利用して、各問のコードが期待通り動作するようなCustomerクラスを実装してください
 
-# 雛形
+
+from inspect import AGEN_CLOSED
+
+
 class Customer:
     # 各問のコードが期待通り動作するように実装
-    pass
+    #コンストラクタ(初期化メソッド)
+    def __init__(self, first_name, family_name, age):
+        self.first_name = first_name
+        self.family_name = family_name
+        self.age = age
+    
+    # C−1メソッド
+    def full_name(self):
+        return f"{self.first_name} {self.family_name}"
+    
+    # C-2メソッド
+    def custmoer_age(self):
+        return f"{self.age}"
+    
+    # C-3・C-5メソッド
+    def entry_fee(self):
+        if self.age  <= 3:
+            entry_fee = 0
+        elif self.age <20:
+            entry_fee = 1000
+        elif self.age >=20 and self.age <= 65:
+            entry_fee = 1500
+        else:
+            entry_fee = 1200
+        return entry_fee
+    
+    # C−4メソッド
+    def info_csv(self):
+        return f"{self.full_name()}",{self.age},{self.entry_fee()}
+
+     # C-6: 75歳以上500円
+    def entry_fee_c6(self):
+        if self.age <= 3:
+            return 0
+        elif self.age < 20:
+            return 1000
+        elif self.age < 65:
+            return 1500
+        elif self.age < 75:
+            return 1200
+        else:
+            return 500
+
+    # C-7 タブ区切りのCSV
+    def info_csv(self):
+        return f"{self.full_name()}\t{self.age}\t{self.entry_fee()}\t"
 
 
 ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
 tom = Customer(first_name="Tom", family_name="Ford", age= 57)
 ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=75)
+michelle = Customer(first_name="Michelle", family_name="Tanner", age=3)
 
 
+# C-1 出力(フルネームで取得できる)
+print("【C-1出力】")
+print(ken.full_name())  # "Ken Tanaka" という値を出力
+print(tom.full_name())  # "Tom Ford" という値を出力
+print(ieyasu.full_name())  # "Ieyasu Tokugawa" という値を出力
 
-# C-1 フルネームで取得できる
-# ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
-# tom = Customer(first_name="Tom", family_name="Ford", age= 57)
-# ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=75)
 
-
-# C-2 年齢という概念の追加
-# print(ken.age)  # 15 という値を出力
-# print(tom.age) # 57 という値を出力
-# print(ieyasu.age) # 75 という値を出力
-
+# C-2 出力(年齢という概念の追加)
+print("【C-2出力】")
+print(ken.age)  # 15 という値を出力
+print(tom.age) # 57 という値を出力
+print(ieyasu.age) # 75 という値を出力
 
 
 # C-3 年令に応じた適切な入場料(entry_fee)を計算できる
@@ -30,17 +79,17 @@ ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=75)
 # こども料金(20歳未満):1000円
 # おとな料金(20歳以上65歳未満):1500円
 # # シニア料金(65歳以上):1200円
-# print(ken.entry_fee())  # 1000 という値を出力
-# print(tom.entry_fee())  # 1500 という値を出力
-# print(ieyasu.entry_fee())  # 1200 という値を出力
-
-
+print("【C-3出力】")
+print(ken.entry_fee())  # 1000 という値を出力
+print(tom.entry_fee())  # 1500 という値を出力
+print(ieyasu.entry_fee())  # 1200 という値を出力
 
 
 # C-4 単一の顧客情報をCSV形式で取得できる
-# print(ken.info_csv())  # "Ken Tanaka,15,1000" という値を出力
-# print(tom.info_csv())  # "Tom Ford,57,1500" という値を出力
-# print(ieyasu.info_csv())  # "Ieyasu Tokugawa,75,1200" という値を出力
+print("【C-4出力】")
+print(ken.info_csv())  # "Ken Tanaka,15,1000" という値を出力
+print(tom.info_csv())  # "Tom Ford,57,1500" という値を出力
+print(ieyasu.info_csv())  # "Ieyasu Tokugawa,75,1200" という値を出力
 
 
 
@@ -48,26 +97,18 @@ ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=75)
 # Customerクラスの実装だけでなく、仕様の理解やどのようにクラスを利用するのかも含めて考えてください
 
 # C-5 3歳以下の入場料の無料化
-# 以下のコードを追加して、期待通りの出力が得られるように実装してください
 
-#   class Customer:
-#       # 省略
-  
-  
-#   ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
-#   tom = Customer(first_name="Tom", family_name="Ford", age= 57)
-#   ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=75)
-# + michelle = Customer(first_name="Michelle", family_name="Tanner", age=3)
-  
-#   # 省略
-#   print(ken.entry_fee())  # 1000 という値を出力
-#   print(tom.entry_fee())  # 1500 という値を出力
-#   print(ieyasu.entry_fee())  # 1200 という値を出力
-#   print(michelle.entry_fee())  # 0 という値を出力
-
+print("【C-5出力】")
+print(ken.entry_fee())  # 1000 という値を出力
+print(tom.entry_fee())  # 1500 という値を出力
+print(ieyasu.entry_fee())  # 1200 という値を出力
+print(michelle.entry_fee())  # 0 という値を出力
 
 
 # C-6 75歳以上の入場料金は500円にしてください
+
+print("【C-6出力】")
+print(ieyasu.entry_fee_c6())
 
 # C-7 単一顧客の情報取得形式の追加その1
 # 単一顧客の情報取得をタブ区切りにも対応させてください
@@ -77,7 +118,8 @@ ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=75)
 # Ieyasu Tokugawa 75      500
 # Michelle Tanner 3       0
 
-
+    for customer in customers:
+        print(customer.info_csv())
 
 
 # C-8 単一顧客の情報取得形式の追加その2
