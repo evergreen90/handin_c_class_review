7d50acec6353be82a32dec2a788e5999d086b0a8
evergreen90 <evergreen90@gmail.com>
2025-08-15 09:06:42 +0900

    firat commit

---
+++ customer.py
@@ -0,0 +1,14 @@
+# 下記の雛形を利用して、各問のコードが期待通り動作するようなCustomerクラスを実装してください
+
+# 雛形
+class Customer:
+    # 各問のコードが期待通り動作するように実装
+    pass
+
+
+ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
+tom = Customer(first_name="Tom", family_name="Ford", age= 57)
+ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=75)
+
+
+
